8c7537ddf808a2204d7c049e42bcbd4568f11fc9
Andrey Batukhtin <batyhtik3@gmail.com>
2023-10-20 13:53:19 +0300

    clear code

--- CSharpModule12/Views/Windows/MainWindow.xaml.cs
+++ CSharpModule12/Views/Windows/MainWindow.xaml.cs
@@ -22,58 +22,5 @@ namespace CSharpModule12.Views.Windows
         {
             InitializeComponent();
         }
-
-        //private void dataGridClients_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        //{
-        //    _currentClient = dataGridClients.SelectedItem as Client;
-
-        //    if (_currentClient.ClientBankAccount != null)
-        //    {
-        //        comboBoxCurrentBankAccount.ItemsSource = _currentClient.ClientBankAccount;
-        //        comboBoxCurrentBankAccount.SelectedIndex = 0; // выбираем в combo box первый существующий счет
-        //    }
-        //}
-
-        //private void comboBoxCurrentBankAccount_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        //{
-        //    if (comboBoxCurrentBankAccount.SelectedItem == null)
-        //    {
-        //        textBlockCurrentBankAccount.Text = "";
-        //        return;
-        //    }
-
-        //    _currentCBankAccount = comboBoxCurrentBankAccount.SelectedItem as BankAccount;
-        //    string statucBankAccount = _currentCBankAccount.IsOpen ? "Открыт" : "Закрыт";
-        //    textBlockCurrentBankAccount.Text = 
-        //        $"ID: {_currentCBankAccount.Id}\n" +
-        //        $"Статус: {statucBankAccount}\n" +
-        //        $"Колличество средств: {_currentCBankAccount.Money}";
-        //}
-
-        //private void openCloseBankAccount_Click(object sender, RoutedEventArgs e)
-        //{
-        //    if(_currentCBankAccount != null)
-        //    {
-        //        _currentCBankAccount.OpenOrCloseBankAccount(!_currentCBankAccount.IsOpen);
-        //    }
-        //}
-
-        //private void addMoney_Click(object sender, RoutedEventArgs e)
-        //{
-        //    if (_currentCBankAccount != null)
-        //    {
-                
-        //    }
-        //}
-
-        //private void transferMoney_Click(object sender, RoutedEventArgs e)
-        //{
-
-        //}
-
-        //private void addNewBankAccount_Click(object sender, RoutedEventArgs e)
-        //{
-
-        //}
     }
 }
